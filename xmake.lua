780b8cba2bc87f1e32593f9e38c4074d999a4643
mono-cyte <a578448983@gmail.com>
2025-09-28 23:21:55 +0800

    shrink xmake.lua

--- xmake.lua
+++ xmake.lua
@@ -2,16 +2,11 @@ add_rules("mode.debug","mode.release")
 
 set_languages("cxx11")
 
-target("common")
-    set_kind("static")
-    set_pcxxheader("include/IPrefix.h")
-    add_files("src/*.cpp")
-    add_includedirs("include")
-    add_headerfiles("include/*.h","include/*.inc")
-
-
 namespace("xse", function () 
     target("common")
-        set_kind("phony")
-        add_deps("common")
+        set_kind("static")
+        set_pcxxheader("include/IPrefix.h")
+        add_files("src/*.cpp")
+        add_includedirs("include")
+        add_headerfiles("include/*.h","include/*.inc")
 end)
