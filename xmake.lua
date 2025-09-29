b2e0538f1c8a534d18e34686b6e3d82b477ed804
mono-cyte <a578448983@gmail.com>
2025-09-29 17:35:38 +0800

    refine xmake info

--- xmake.lua
+++ xmake.lua
@@ -4,16 +4,16 @@ add_rules("mode.debug","mode.release")
 
 namespace("xse", function () 
     target("common")
-    
-        set_languages("cxx11")
+        set_languages("c++11")
         set_plat("windows")
         set_arch("x64")
+        set_toolchains("msvc")
 
         set_kind("static")
         set_pcxxheader("include/IPrefix.h")
         add_files("src/*.cpp")
         add_includedirs("include")
-        add_headerfiles("include/*.h","include/*.inc")
+        add_headerfiles("include/(*.h)","include/(*.inc)")
         set_version("2.0.0")
 
 end)
