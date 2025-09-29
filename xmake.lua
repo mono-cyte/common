7c61ee00aebe2e40ddcf32df5c660aa8d514888e
mono-cyte <a578448983@gmail.com>
2025-09-29 12:41:27 +0800

    Restrict build environment to Windows x64

--- xmake.lua
+++ xmake.lua
@@ -1,12 +1,19 @@
 add_rules("mode.debug","mode.release")
 
-set_languages("cxx11")
+
 
 namespace("xse", function () 
     target("common")
+    
+        set_languages("cxx11")
+        set_plat("windows")
+        set_arch("x64")
+
         set_kind("static")
         set_pcxxheader("include/IPrefix.h")
         add_files("src/*.cpp")
         add_includedirs("include")
         add_headerfiles("include/*.h","include/*.inc")
+        set_version("2.0.0")
+
 end)
